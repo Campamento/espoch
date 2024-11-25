584ba74a81951272087f9c420c3ebf8fb8d50a9b
unknown <fernandaminga42@gmail.com>
2024-11-25 14:29:44 -0500

    Mi primer commit

---
+++ src/main/java/com/mycompany/usuariosespoch/UsuariosEspoch.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.usuariosespoch;
+
+/**
+ *
+ * @author Lizeth
+ */
+public class UsuariosEspoch {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
